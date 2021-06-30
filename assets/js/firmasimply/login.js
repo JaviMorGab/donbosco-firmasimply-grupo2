c44de84d53923403f138e4adfe9e4bdbcd90670c
Derian <derian12702@gmail.com>
2021-06-30 15:21:54 +0200

    .

--- assets/js/firmasimply/login.js
+++ assets/js/firmasimply/login.js
@@ -1,20 +1,28 @@
 import Auth from './Modules/Auth/Auth.js';
 
+
+
 const DASHBOARD_URL = '/index.html';
-const CODER = {email: 'maria@firmasimply.com', password: 'password'};
+
 
 // Login
 
 const btnLogin = document.querySelector('#login');
 btnLogin.addEventListener('click', login, false);
 
-async function login() {
+async function login(e) {
+  e.preventDefault()
+  let email = document.getElementById("username").value;
+  let password = document.getElementById("password").value;
+  const CODER = { email: email, password: password };
   await Auth.login(CODER);
   window.location.assign(DASHBOARD_URL);
 }
+
 
 // Consultar los datos del usuario logeado
 Auth.getCoder();
 
 // Consultar el token guardado
 Auth.getToken();
+
--- assets/js/firmasimply/tareas.js
+++ assets/js/firmasimply/tareas.js
@@ -2,54 +2,15 @@ import Auth from './Modules/Auth/Auth.js';
 import Tarea from './Modules/Tarea.js';
 import Categoria from './Modules/Categoria.js';
 
-// // Funcionalidad mostrar listado de tareas
-// Tarea.getListadoTareas();
+let contadorTareas = 0;
+let contadorCompletadas = 0;
+let contadorPendientes = 0;
 
-// // Funcionalidad crear una tarea
-// let tarea = {
-// 	titulo: 'Tarea nueva',
-// 	descripcion: 'Lorem Ipsum',
-// 	estado: 0, // 0 pendiente, 1 completada
-// 	user_id: Auth.getCoder().id, // esta funcion devuelve el id del coder logeado
-// 	categoria_id: 1,
-// };
-// class Store {
-// 	static añadirTarea(task) {
-//         const tasks = Store.getTasks();
-//         tasks.push(task);
-//         localStorage.setItem('tasks', JSON.stringify(tasks));
-//     }
-// 	static borrarTarea(isbn) {
-//         const books = Store.getBooks();
-//         books.forEach((book, index) => {
-//             if(book.isbn === isbn){
-//                 books.splice(index, 1);
-//             }
-//         });
-//         localStorage.setItem('books', JSON.stringify(books));
-//     }
-// 	static getBooks() {
-//         let books = localStorage.getItem('books');
-//         if(books === null) {
-//             return [];
-//         } else {
-//             return JSON.parse(books);
-//         }
-//     }
+// let isBoss = confirm("¿Seguro que quieres marcarlo como completado?");
 
-// }
+// console.log( isBoss ); // true si se pulsa Aceptar, false si se pulsa Cancelar	
 
-// Tarea.crearTarea(tarea);
-// // Funcionalidad de borrar una tarea
-// let idTarea = 2;
-// Tarea.borrarTarea(idTarea);
 
-// // Funcionalidad de marcar una tarea como completada o pendiente
-
-// let data = { estado: 1 }; // 1 completada, 0 pendiente
-// Tarea.marcarTarea(data, idTarea);
-// // Funcionalidad de cargar las categorias en el formulario de crear la tarea
-// Categoria.getListadoTareas();
 
 
 class task {
@@ -60,58 +21,72 @@ class task {
 		this.fechaDeCreacion = fechaDeCreacion;
 	}
 }
-const mostrarTareas = [
-	{
+	const defaultTareas = [
+		{
 		nombreTarea: 'Periodico',
-		categoria: 'html',
+		categoria: 'HTML',
 		descripcion: 'Hacerlo Responsive',
 		fechaDeCreacion : '2021-06-12'
 	},
 	{
 		nombreTarea: 'Cajas',
-		categoria: 'css',
+		categoria: 'CSS',
 		descripcion: 'Usar flex',
 		fechaDeCreacion : '2021-04-12'
 	}
 ]
 class UI {
 	static mostrarTareas() {
-		mostrarTareas.forEach(tarea => UI.añadirTarea(tarea))
+		defaultTareas.forEach(tarea => UI.addTareaToList(tarea))
 	}
-	static añadirTarea(tarea) {
+	static addTareaToList(tarea) {
 		const list = document.getElementById('listaTareas')
 		const row = document.createElement('tr')
 		row.innerHTML = `
-		<th scope="row"><input type="checkbox" /></th>
+		<th scope="row"><input type="checkbox" id="checkTareas" checked/></th>
 		<td>${tarea.nombreTarea}</td>
 		<td>${tarea.categoria}</td>
 		<td>${tarea.descripcion}</td>
 		<td>${tarea.fechaDeCreacion}</td>
-		<td><a class="tm-product-delete-link" id="borrar	"><i class="far fa-trash-alt tm-product-delete-icon"></i></a></td>
-		`
-		list.appendChild(row)
+		<td><a href="#" class="tm-product-delete-link" id="checkbox"><i class="far fa-trash-alt tm-product-delete-icon borrar"></i></a></td>
+		`;
+		list.appendChild(row);
+		let isChecked = document.getElementById('checkTareas').checked;
+		if(isChecked){
+			alert('checkbox esta seleccionado');
+			contadorCompletadas++;
+			console.log("completadas: " + contadorCompletadas);
+		}
+		contadorTareas++;
+		console.log(contadorTareas);
 	}
-	static clearfields(tarea) {
+	static limpiarCampos() {
 		document.getElementById('nombreTarea').value = '';
 		document.getElementById('categoria').value = '';
 		document.getElementById('descripcion').value = '';
 		document.getElementById('fechaDeCreacion').value = '';
 	}
 	static borrarTarea(target) {
-        if(target.classList.contains('borrar'))  {
-            target.parentElement.parentElement.remove();
+		console.log(target.parentElement.parentElement);
+		let isChecked = document.getElementById('checkTareas').checked;
+		console.log(isChecked);
+        if(target.classList.contains('borrar') && isChecked)  {
+            target.parentElement.parentElement.parentElement.remove();
+			contadorTareas--;
+			console.log(contadorTareas);
          }
     }
 }
-UI.mostrarTareas()
 
-// Event: Add a Book
-document.querySelector('#formularioTareas').addEventListener('submit', añadirTarea, false)
+UI.mostrarTareas();
+
+// Event: Add a Tarea
+document.querySelector('#formularioTareas').addEventListener('submit', añadirTarea, false);
 
 function añadirTarea(e) {
 	// prevent actual submission
-	e.preventDefault()
-
+	e.preventDefault();
+	
 	// Capturar los valores del Form
 	const nombreTarea = document.getElementById('nombreTarea').value;
 	const descripcion = document.getElementById('descripcion').value;
@@ -119,15 +94,17 @@ function añadirTarea(e) {
 	const fechaDeCreacion = document.getElementById('fechaDeCreacion').value;
 
 	// Crear un nuevo objeto book
-	const tarea = new task(nombreTarea, descripcion, categoria, fechaDeCreacion)
+	const tarea = new task(nombreTarea, descripcion, categoria, fechaDeCreacion);
 	// Añadir el objeto book creado a UI (mostrarlo en HTML)
-	UI.añadirTarea(tarea);
-	UI.clearfields();
+	UI.addTareaToList(tarea);
+	UI.limpiarCampos();
 
 }
 
-document.getElementById('tarea').addEventListener('click', handleRemove)
+document.getElementById('listaTareas').addEventListener('click', handleRemove);
 function handleRemove(e) {
-  UI.borrarTarea(e.target)
+  UI.borrarTarea(e.target);
 }
+
+
 
