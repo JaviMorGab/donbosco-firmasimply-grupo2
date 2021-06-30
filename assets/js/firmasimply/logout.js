046f4faafbf1fef1602317c320d8d1236880bf1d
Derian <derian12702@gmail.com>
2021-06-30 15:19:34 +0200

    funcionamiento de borrar y empiezo del estado con el checbkox, login conectado junto con logout, limpieza de codigo de pildora

--- assets/js/firmasimply/logout.js
+++ assets/js/firmasimply/logout.js
@@ -1,13 +1,14 @@
 import Auth from './Modules/Auth/Auth.js';
 
-Auth.logout();import Auth from './Modules/Auth/Auth.js';
+//Auth.logout();import Auth from './Modules/Auth/Auth.js';
 
-if(document.querySelector('#logout') != null) {
+if (document.querySelector('#logout') != null) {
   const btnLogout = document.querySelector('#logout');
   btnLogout.addEventListener('click', logout, false);
 }
 
-async function logout() {
+async function logout(e) {
+  e.preventDefault()
   await Auth.logout();
   window.location.assign("http://127.0.0.1:5500/login.html");
 }
