311b8378a81e5a4bb1257ebfad1e98ec92813521
Derian <derian12702@gmail.com>
2021-07-02 15:28:46 +0200

    cambio en el archivo pildoras js

--- assets/js/firmasimply/pildoras.js
+++ assets/js/firmasimply/pildoras.js
@@ -1,6 +1,40 @@
 import Auth from './Modules/Auth/Auth.js';
 import Pildora from './Modules/Pildora.js';
 
+// async function getListadoPildoras() {
+//     let res = await Pildora.getListadoPildoras();
+//     console.log(res)
+// }
+
+// // Funcionalidad crear píldora
+// //
+// let pildora = {
+//     nombre: 'NPM',
+//     descripcion: 'Lorem Ipsum',
+//     fecha_presentacion: '2021-07-12',
+//     estado: 0, // 0 pendiente, 1 presentada
+//     user_id: Auth.getCoder().id, // esta funcion devuelve el id del coder logeado
+// };
+
+// Pildora.crearPildora(pildora);
+
+// // Funcionalidad borrar píldora
+// //
+// let idPildora = 2;
+// Tarea.borrarPildora(idPildora);
+
+// // Funcionalidad marcar una píldora como presentada o pendiente
+// //
+// let idPildora = 2;
+// let data = { estado: 1 }; // 1 presentada, 0 pendiente
+// Tarea.marcarPildora(data, idPildora);
+
+
+// // Módulo Píldoras
+
+// // Consultar el listado de píldoras
+// Pildora.getListadoPildoras();
+
 class Book {
     constructor(title,author,fechaini,fechap,estado){
         this.title = title;
@@ -72,10 +106,10 @@ class UI {
         // añadir el DIV en el DOM
         const container = document.querySelector('.container');
         const form = document.querySelector('#book-form');
-        // container.insertBefore(div,form);
+        container.insertBefore(div,form);
 
-         // Mostrar las alarmas durante 3 segundos
-        // setTimeout(() => document.querySelector('.alert').remove(), 3000);
+        // Mostrar las alarmas durante 3 segundos
+        setTimeout(() => document.querySelector('.alert').remove(), 3000);
     }
 }
 
